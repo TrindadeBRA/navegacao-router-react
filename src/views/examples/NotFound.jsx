05ce39dacaf1bbec2a04a12a1225ec6a1e8c05d4
Lucas Trindade <trindadebra@gmail.com>
2023-05-17 12:01:52 -0300

    first commit

---
+++ src/views/examples/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const NotFound = props => {
+    return (
+        <div className="NotFound">
+            <h1>Componente NotFound</h1>
+            <p>Opsss 404!</p>
+        </div>
+    )
+}
+
+export default NotFound
